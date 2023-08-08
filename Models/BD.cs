bf436542101d3d43481d87f26761952ec39f34cc
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2023-08-08 10:18:43 -0300

    BD AVANCES

--- Models/BD.cs
+++ Models/BD.cs
@@ -9,4 +9,43 @@ namespace TP07_CZERNUSZKA_KOZIUPA.Models;
 
 public static class BD{
     private static string _connectionString = @"Server=localhost;DataBase=PreguntadOrt;Trusted_Connection=True;";
+
+    public static List<Categorias> ObtenerCategorias(){
+        List<Categorias> listaCategorias = null;
+        using(SqlConnection BD = new SqlConnectionQ(_connectionString)){
+            string sql = "";
+            listaCategorias = BD.Query<Categorias>(sql).ToList();
+        }
+        return listaCategorias;
+    }
+
+    public static List<Dificultades> ObtenerDificultades(){
+        List<Dificultades> listaDificultades = null;
+        using(SqlConnection BD = new SqlConnection(_connectionString)){
+            string sql "";
+            listaDificultades = BD.Query<Dificultades>(sql).ToList();
+        }
+        return listaDificultades;
+    }
+
+    public static List<Preguntas> ObtenerPreguntas(int IdDificultad, int IdCategoria){        
+        using(SqlConnection BD = new SqlConnection(_connectionString))
+        {
+            string sql = "SELECT * FROM Preguntas";
+            DynamicParameters parametros = new DynamicParameters();
+
+            if(IdDificultad != -1)
+            {
+                sql += " WHERE IdDificultad = @pIdDificultad";
+                parametros.Add("@pIdDificultad", IdDificultad);
+            }
+            if(IdCategoria != -1)
+            {
+                sql += (IdDificultad != -1) ? " AND" : " WHERE";
+                sql += " IdCategoria = @pIdCategoria";
+                parametros.Add("@pIdCategoria", IdCategoria);
+            }
+
+            return BD.Query<Preguntas>(sql, parametros).ToList();  
+        }
 }
